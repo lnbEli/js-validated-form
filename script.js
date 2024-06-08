6d0a38faf38e83e493407482dd04e49a18dba5cc
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-06-08 12:53:07 +0200

    JS to set error messag ewhen field not valid

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-
+function setErrorMessage(input) {
+  const errorMessage = document.querySelector(`${input.id}-error`);
+  if (input.validity.typeMismatch) {
+    errorMessage.textContent = `Entered value needs to be a ${input.id}`;
+  } else if (input.validity.valueMissing) {
+    errorMessage.textContent = "Please enter info.";
+  } else if (input.validity.patternMismatch) {
+    errorMessage.textContent = `Please make sure value is in ${input.id} format`;
+  } else if (
+    input.validity.patternMismatch &&
+    errorMessage.type === "password"
+  ) {
+    errorMessage.textContent =
+      "Password to have min. 8 characters, one number, one letter and one special character";
+  }
+}
