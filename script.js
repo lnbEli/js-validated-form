d3bb5bef2ebbad2ac2de3a7917099717fb57051f
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-06-11 18:45:08 +0200

    Css valid/invalid classes wit has() and JS to only start indicationing to user that field is to be filled once they have focused on it

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 function setErrorMessage(input) {
-  const errorMessage = document.querySelector(`${input.id}-error`);
+  const errorMessage = document.querySelector(`.${input.id}-error`);
   if (input.validity.typeMismatch) {
-    errorMessage.textContent = `Entered value needs to be a ${input.id}`;
+    errorMessage.textContent = `Entered value needs to be an ${input.id}`;
   } else if (input.validity.valueMissing) {
-    errorMessage.textContent = "Please enter info.";
+    errorMessage.textContent = "Please enter value";
+  } else if (input.validity.patternMismatch && input.type === "password") {
+    errorMessage.textContent =
+      "Password to have min. 8 characters, one number, one letter and no special characters";
   } else if (input.validity.patternMismatch) {
     errorMessage.textContent = `Please make sure value is in ${input.id} format`;
-  } else if (
-    input.validity.patternMismatch &&
-    errorMessage.type === "password"
-  ) {
-    errorMessage.textContent =
-      "Password to have min. 8 characters, one number, one letter and one special character";
   }
 }
+
+const formInputs = document.querySelectorAll("input");
+
+formInputs.forEach((input) => {
+  const errorMessage = document.querySelector(`.${input.id}-error`);
+
+  //   adds required attribute and class once input has received focus
+  input.addEventListener("focus", () => {
+    input.required = true;
+    input.classList.add("user-focused");
+  });
+
+  //adds error message or class depending on validity
+  input.addEventListener("blur", () => {
+    if (input.validity.valid) {
+      errorMessage.textContent = "";
+      errorMessage.className = `${input.id}-error`;
+    } else {
+      setErrorMessage(input);
+    }
+  });
+});
