8306a533f61dad7e25fcc2810b766c37e0c61d32
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-06-12 20:18:16 +0200

    function that checks passwords are the same  when form submitted. Add smiley to indicate form item completed correctly

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Open modal and submit buttons
+const closeButton = document.querySelector(".close-button");
+const submitButton = document.querySelector(".submit");
+
+closeButton.addEventListener("click", closeModal);
+submitButton.addEventListener("click", submitForm);
+
 function setErrorMessage(input) {
   const errorMessage = document.querySelector(`.${input.id}-error`);
   if (input.validity.typeMismatch) {
@@ -12,24 +19,99 @@ function setErrorMessage(input) {
   }
 }
 
-const formInputs = document.querySelectorAll("input");
+function manageFocus(event) {
+  const input = event.target;
+  input.required = true;
+  input.classList.add("user-focused");
+}
 
-formInputs.forEach((input) => {
+function manageBlur(event) {
+  const input = event.target;
   const errorMessage = document.querySelector(`.${input.id}-error`);
+  const passOne = document.querySelector("#password-one");
+  const passTwo = document.querySelector("#password-two");
+  const passOneError = document.querySelector(".password-one-error");
+  const passTwoError = document.querySelector(".password-two-error");
 
-  //   adds required attribute and class once input has received focus
-  input.addEventListener("focus", () => {
-    input.required = true;
-    input.classList.add("user-focused");
+  if (
+    checkPasswordsAreEqual() &&
+    passOne.validity.valid &&
+    passTwo.validity.valid
+  ) {
+    passOneError.textContent = "";
+    passTwoError.textContent = "";
+    passOneError.className = "password-one-error";
+    passTwoError.className = "password-two-error";
+  } else if (input.validity.valid) {
+    errorMessage.textContent = "";
+    errorMessage.className = `${input.id}-error`;
+  } else {
+    setErrorMessage(input);
+  }
+}
+
+//adds eventlisteners to inputs
+function addEventListenerToInputs() {
+  const formInputs = document.querySelectorAll("input");
+
+  formInputs.forEach((input) => {
+    //   adds required attribute and class once input has received focus
+    input.addEventListener("focus", manageFocus);
+
+    //adds error message or class depending on validity
+    input.addEventListener("blur", manageBlur);
   });
+}
+addEventListenerToInputs();
 
-  //adds error message or class depending on validity
-  input.addEventListener("blur", () => {
-    if (input.validity.valid) {
-      errorMessage.textContent = "";
-      errorMessage.className = `${input.id}-error`;
-    } else {
-      setErrorMessage(input);
+function closeModal() {
+  location.reload();
+}
+
+function submitForm() {
+  const modal = document.querySelector(".modal-background");
+  const form = document.querySelector("form");
+
+  if (form.checkValidity() && checkPasswordsAreEqual()) {
+    modal.style.display = "block";
+  } else {
+    checkInputsHaveBeenFocussedBeforeSubmit();
+    setCheckPasswordsErrorMessages();
+    return;
+  }
+}
+
+function setCheckPasswordsErrorMessages() {
+  const passwordInputs = document.querySelectorAll('input[type="password"]');
+  passwordInputs.forEach((input) => {
+    const errorMessages = document.querySelectorAll(
+      ".password-one-error,.password-two-error"
+    );
+    if (!checkPasswordsAreEqual()) {
+      errorMessages.forEach((error) => {
+        error.textContent = "Please make sure passwords are equal";
+      });
     }
   });
-});
+}
+
+// This focusses on all inputs to make sure that any warning messages are displayed
+function checkInputsHaveBeenFocussedBeforeSubmit() {
+  const formInputs = document.querySelectorAll("input");
+  const submitButton = document.querySelector(".submit");
+  formInputs.forEach((input) => {
+    input.focus();
+  });
+  submitButton.focus();
+}
+
+//Check passwords match
+function checkPasswordsAreEqual() {
+  const passOne = document.querySelector("#password-one");
+  const passTwo = document.querySelector("#password-two");
+  if (passOne.value === passTwo.value) {
+    return true;
+  } else {
+    return false;
+  }
+}
